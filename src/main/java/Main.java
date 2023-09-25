f0a8be65f65ccf8cabc7f7ee3ca444fa8c35a363
theaskou <thsk0001@stud.kea.dk>
2023-09-25 13:23:14 +0200

    :)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Thea!");
         System.out.println("Håber du har haft en god weekend!");
+        System.out.println("Tak!");
     }
 }
