e0fddada3ffb634e2e3a19724c853aa5afffcee0
MuLangballe <mala0007@stud.kea.dk>
2023-09-25 13:26:03 +0200

    ;)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Hello Thea!");
         System.out.println("Håber du har haft en god weekend!");
         System.out.println("Tak!");
+        System.out.println("Håber at alkoholen ramte fedt i fredags ;)");
+        System.out.println(":)");
     }
 }
