4386126f33c156826318c62d8e08d7fbc7a2d160
MuLangballe <mala0007@stud.kea.dk>
2023-09-25 13:17:30 +0200

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Thea!");
+        System.out.println("Håber du har haft en god weekend!");
     }
 }
